92115f97b2c708e800cbf2429021fbca132928b8
Nora Sandler <nsandler@securityinnovation.com>
2012-08-14 13:01:19 -0700

    added files for eventual unit tests

--- YASATEngine/Properties/Settings.Designer.cs
+++ YASATEngine/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace YASATEngine.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Rules")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Users\\Nora Sandler\\YASAT\\Rules")]
         public string RulesDirectory {
             get {
                 return ((string)(this["RulesDirectory"]));
---
+++ YASATTests/RuleTest.cs
@@ -0,0 +1,87 @@
+﻿using YASATEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace YASATTests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for RuleTest and is intended
+    ///to contain all RuleTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class RuleTest
+    {
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        /// Test each rules file, checking how well it catches real issues
+        [TestMethod()]
+        public void SQLiRealIssues()
+        {
+            RuleManager ruleGetter = new RuleManager();
+            List<Rule> rules = GetRulesFromFile(YASATEngine.Settings.Default.RulesDirectory + "\\" + Settings.Default.SQLi); 
+            string fileToTest = TestContext.DataRow["RealIssuesFile"]; //Create a file with a SQLi problem in it.
+            actual = target.FindIssues(fileToTest);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// Test each rules file, checking how well it avoids false positives
+        [TestMethod()]
+        public void AvoidFalseTest()
+        {
+            /// fill in later
+        }
+    }
+}
